204ebb856b82f7b9f1405490cc6570c044cf62a4
dev.demalee@gmail.com <dev.demalee@gmail.com>
2020-10-07 22:31:56 +0300

    javascript update

--- resources/views/billing/index.blade.php
+++ resources/views/billing/index.blade.php
@@ -7,9 +7,10 @@
                 <div class="form-body">
 
                     <div class="form-actions">
-                        <button type="submit"class="btn btn-success"  id="btnSubmit"  >
-                            <i class="icon-user "></i><a href="#"> Top up your wallet</a>
+                        <button type="button" class="btn btn-success mr-1" data-toggle="modal" data-target="#topup">
+                            <i class="icon-user "></i>  Top Up
                         </button>
+
                         <button type="button" class="btn btn-success mr-1">
                             <i class="icon"></i> <a href="#">Safaricom: 2</a>
                         </button>
@@ -131,7 +132,58 @@
                     </div>
                 </div>
             </div>
+            <div class="modal fade text-left" id="topup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel35" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title" id="myModalLabel35"> Top Up Company</h3>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form>
+                            <div class="modal-body">
+                                <fieldset class="form-group floating-label-form-group">
+                                    <label for="email">Company name</label>
+                                    <input type="text" class="form-control" name="email" id="email" placeholder="name">
+                                </fieldset>
+                                <br>
+                                <fieldset class="form-group floating-label-form-group">
+                                    <label for="title">Company Phone</label>
+                                    <input type="tel" class="form-control" name="msisdn" id="phone" placeholder="phone">
+                                </fieldset>
+                                <br>
+                                <fieldset class="form-group floating-label-form-group">
+                                    <label for="title">Amount</label>
+                                    <input type="text" class="form-control" name="amount"  placeholder="address">
+                                </fieldset>
+                                <br>
+
+                            </div>
+                            <div class="modal-footer">
+                                <input type="reset" class="btn btn-outline-secondary btn-lg" data-dismiss="modal" value="close">
+                                <input type="submit" class="btn btn-outline-primary btn-lg" value="Top Up">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </section>
     </inbox>
+    <script src="../app-assets/vendors/js/core/jquery-3.3.1.min.js"></script>
+    <script src="../app-assets/vendors/js/core/popper.min.js"></script>
+    <script src="../app-assets/vendors/js/core/bootstrap.min.js"></script>
+
+
+    <!-- BEGIN PAGE VENDOR JS-->
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN CONVEX JS-->
+    <script src="../app-assets/js/app-sidebar.js"></script>
+    <script src="../app-assets/js/notification-sidebar.js"></script>
+    <script src="../app-assets/js/customizer.js"></script>
+    <!-- END CONVEX JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="../app-assets/js/components-modal.min.js"></script>
 @endsection
 
